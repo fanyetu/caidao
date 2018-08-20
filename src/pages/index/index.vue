f591dfc38320955714525f032639810a17c3c85c
fanyetu <hnan187@126.com>
2018-08-21 00:53:49 +0800

    commit

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,29 +2,63 @@
   <div class="container">
     <div class="top">
       <div class="title">{{title}}</div>
-      <div class="expend"></div>
-      <div class="income-wrapper"></div>
+      <div>
+        <div class="expend">
+          <div class="text">本月支出</div>
+          <div class="month-expend">{{monthExpend}}</div>
+        </div>
+        <div class="income">
+          <div>
+            <span class="text">本月收入</span>
+            <span class="month-income">{{monthIncome}}</span>
+          </div>
+          <div>
+            <span>本月可用</span>
+            <span class="month-available">{{monthAvailable}}</span>
+          </div>
+        </div>
+      </div>
+      <!--<img class="avatar" :src="avatarUrl"/>-->
     </div>
     <div class="middle">
-      <button type="primary">记一笔</button>
+      <button type="primary" @click="addNote">记一笔</button>
     </div>
     <div class="bottom"></div>
   </div>
 </template>
 <script>
 
-export default {
-  data () {
-    return {
-      title: '家庭账本'
+  export default {
+    data() {
+      return {
+        avatarUrl: "",
+        title: "家庭账本",
+        monthExpend: "0.00",
+        monthIncome: "0.00",
+        monthAvailable: "10000.00"
+      };
+    },
+    created() {
+      let _this = this
+      wx.showLoading({
+        title: "加载中"
+      });
+      wx.getUserInfo({
+        success: function(res) {
+          console.log(res.userInfo);
+          _this.avatarUrl = res.userInfo.avatarUrl;
+        },
+        complete: function() {
+          wx.hideLoading();
+        }
+      });
+    },
+    methods: {
+      addNote() {
+        console.log("to add note");
+      }
     }
-  },
-  created(){
-    console.log("created")
-  },
-  methods: {
-  }
-}
+  };
 </script>
 <style scoped lang="less">
   @import "../../common/css/variable";
@@ -37,14 +71,44 @@ export default {
     justify-content: space-between;
     /*padding: 200rpx 0;*/
     box-sizing: border-box;
-    .top{
+    background-color: @bg-color;
+    .top {
       width: 100%;
-      height:400rpx;
-      background-color: @theme-color;
+      background: url("../../common/img/index_bg.jpg") no-repeat center;
+      background-size: 100% 100%;
       box-sizing: border-box;
       padding: @padding-normal;
+      display: flex;
+      justify-content: space-between;
+      flex-direction: column;
+      color: @front-color;
+      .title {
+        flex: 0 0 150 rpx;
+      }
+      .expend {
+        margin-bottom: @padding-normal;
+        .text {
+          font-size: @font-size-small;
+        }
+        .month-expend {
+          font-size: @font-size-largest;
+        }
+      }
+      .income {
+        font-size: @font-size-small;
+        display: flex;
+        justify-content: space-between;
+      }
+      /*.avatar {
+        position: absolute;
+        right: @padding-normal;
+        top: @padding-normal;
+        height: 50 rpx;
+        width: 50 rpx;
+        border-radius: 50%;
+      }*/
     }
-    .middle{
+    .middle {
       width: 100%;
       box-sizing: border-box;
       padding: @padding-normal;
