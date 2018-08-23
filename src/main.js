7b10766dcf796470a1859541a0dbc9070bfc263c
fanyetu <hnan187@126.com>
2018-08-23 22:20:16 +0800

    commit

--- src/main.js
+++ src/main.js
@@ -6,3 +6,20 @@ App.mpType = 'app'
 
 const app = new Vue(App)
 app.$mount()
+
+export default {
+  // 相当于app.json
+  config:{
+    "pages": [
+      "pages/logs/main",
+      "pages/index/main",
+      "pages/counter/main"
+    ],
+    "window": {
+      "backgroundTextStyle": "light",
+      "navigationBarBackgroundColor": "#f8f8f8",
+      "navigationBarTitleText": "财道",
+      "navigationBarTextStyle": "black"
+    }
+  }
+}
